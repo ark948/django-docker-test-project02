160d54702456a58e3d76b6302f8fb2bdfdf41bbd
ark948 <armankarimi948@gmail.com>
2024-07-06 18:46:59 +0330

    Added about page + tests

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from pages.views import HomePageView
+from pages.views import HomePageView, AboutPageView
 
 app_name = "pages"
 urlpatterns = [
+    path("about/", AboutPageView.as_view(), name="about"),
     path("", HomePageView.as_view(), name="index"),
 ]
